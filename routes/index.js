e1cdc130824587dbdbd995cbb2350f8fcc911518
SandraHdz2 <azalia.noir@gmail.com>
2019-05-21 15:58:26 -0500

    BS 4

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hola', function(req, res, next) {
+	//solicitud de una ruta de api rest
+  res.render('saludo', { otraCosa: 'cosita' });
+});
 module.exports = router;
